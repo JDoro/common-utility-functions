6e74fca7db96db270c0625cd81c13b4e2ed510c9
JDoro <josh.j.doro@gmail.com>
2024-04-03 21:01:49 -0500

    added some more simple jsdocs for more of the functions

--- src/range.js
+++ src/range.js
@@ -1,3 +1,9 @@
+/**
+ * Generate a range of integers
+ * @param min - The inclusive minimum value
+ * @param max - The inclusive maximum value
+ * @returns {number[]} a list of integers from min to max
+ */
 export function range (min, max) {
   const length = max - min;
   return [...Array.of(length).keys()].map(i => i + min);
--- src/unary.js
+++ src/unary.js
@@ -1,3 +1,8 @@
+/**
+ * Takes a function that accepts multiple arguments and returns a new function that only accepts one argument.
+ * @param fn - The function to convert to unary
+ * @returns {function(x):x} a new function that only accepts one argument}
+ */
 function unary(fn) {
   return function(a) {
     return fn(a);
