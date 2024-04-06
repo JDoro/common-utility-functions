1705d3042be4a8013d1ffc545f599fb9b8f0c7eb
Joshua Doro <Josh.j.doro@gmail.com>
2024-04-06 10:46:48 -0500

    small improvements to toMap and someProperties

--- src/someProperties.js
+++ src/someProperties.js
@@ -1,3 +1,9 @@
+/**
+ * This function returns a new object with only the properties specified in the properties array
+ * @param {Object.<string, object>} obj
+ * @param properties {Array.<string>}
+ * @returns {Object.<string, object>}
+ */
 export function pickProperties(obj, properties) {
   return properties.reduce((acc, property) => {
     if (obj.hasOwnProperty(property)) {
